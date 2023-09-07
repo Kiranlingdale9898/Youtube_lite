e5b8d8051ee32845f3515abc33692ce8770615d0
Kiran Lingdale <kiranlingdale9898@gmail.com>
2023-09-07 11:53:43 +0530

    Deploying Project Youtube

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":'be702e688dmsh758b9265657e51ap12f11cjsn741031263a3f',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
